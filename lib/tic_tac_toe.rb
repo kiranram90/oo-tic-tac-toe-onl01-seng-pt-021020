b515b397a38a3503f0102bf09ced14abfa094310
kiran ram <kram@lions.molloy.edu>
2020-03-24 01:48:22 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,25 @@
+class TicTacToe
+  
+  def initialize
+    @board = []
+  end
+  
+  WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5],
+  [5,6,7],
+  [0,4,7],
+  [2,4,6],
+  [0,3,5],
+  [1,4,6],
+  [2,5,7]
+  ]
+  
+  def display_board
+    
+  end
+  
+  def input_to_index
+  end
+  
+end
